301297874a1297ee391d3de94e015cadd1861133
yusufferdogan <yusuffx03@gmail.com>
2023-07-12 15:09:41 +0300

    change decimal erc20

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,13 +2,14 @@
 pragma solidity ^0.8.13;
 
 import "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract Token is ERC20 {
-    constructor(uint256 amount) ERC20("Token", "F_TOKEN") {
+contract Token is ERC20, ERC20Burnable {
+    constructor(uint256 amount) ERC20("NXTU", "4 Next Unicorn") {
         _mint(msg.sender, amount);
     }
 
     function decimals() public pure override returns (uint8) {
-        return 1;
+        return 8;
     }
 }
