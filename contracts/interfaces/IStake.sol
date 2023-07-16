fb23fceaccbd97c3a04d0692e57b5e6132fa645b
yusufferdogan <yusuffx03@gmail.com>
2023-07-16 14:35:55 +0300

    update stake interface

--- contracts/interfaces/IStake.sol
+++ contracts/interfaces/IStake.sol
@@ -9,12 +9,22 @@ interface IStake {
         uint256 reward;
     }
 
-    event Staked(address indexed user, uint256 amount);
-    event Unstaked(address indexed user, uint256 amount);
+    event Staked(
+        uint256 indexed projectId,
+        address indexed user,
+        uint256 amount
+    );
+    event Unstaked(
+        uint256 indexed projectId,
+        address indexed user,
+        uint256 amount
+    );
 
     error TransferError();
     error NoDeposit();
     error StakeIsLocked();
     error InsufficientReward();
     error AmountCantBeZero();
+    error AlreadyApproved();
+    error StakeIsEnded();
 }
