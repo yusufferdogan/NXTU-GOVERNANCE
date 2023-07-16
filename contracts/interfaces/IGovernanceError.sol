a42be42a4f99494083bab26549a6b2cc0c946e1e
yusufferdogan <yusuffx03@gmail.com>
2023-07-16 14:35:42 +0300

    add governance interface for events

---
+++ contracts/interfaces/IGovernanceError.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+interface IGovernanceError {
+    error LessThanMinAmount();
+    error ProjectDoesNotExist();
+    error AlreadyVoted();
+    error VoteEnded();
+    error AlreadyApproved();
+    error DidNotApplied();
+
+    event ProjectApplied(uint256 indexed id);
+    event ProjectApproved(
+        uint256 indexed id,
+        string name,
+        string description,
+        string url,
+        uint256 apr,
+        uint256 lockedTime,
+        uint256 voteEndDate,
+        uint256 stakeEndDate
+    );
+    event ProjectVoted(
+        uint256 indexed projectId,
+        address indexed user,
+        bool votedFor
+    );
+}
